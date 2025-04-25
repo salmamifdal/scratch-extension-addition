3ea6b42a6446a3c4db47d925d4135f6257cb37e5
salmamifdal <salmamifdal72@gmail.com>
2025-04-25 13:40:24 -0400

    Add files via upload

--- addition_extension.js
+++ addition_extension.js
@@ -1,31 +1,32 @@
 class MyExtension {
-  getInfo() {
-    return {
-      id: 'addition',
-      name: 'Addition',
-      blocks: [
-        {
-          opcode: 'addNumbers',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'add [NUM1] and [NUM2]',
-          arguments: {
-            NUM1: {
-              type: Scratch.ArgumentType.NUMBER,
-              defaultValue: 1
-            },
-            NUM2: {
-              type: Scratch.ArgumentType.NUMBER,
-              defaultValue: 2
+    getInfo() {
+      return {
+        id: 'addition',
+        name: 'Addition',
+        blocks: [
+          {
+            opcode: 'addNumbers',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'add [NUM1] and [NUM2]',
+            arguments: {
+              NUM1: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: 1
+              },
+              NUM2: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: 2
+              }
             }
           }
-        }
-      ]
-    };
+        ]
+      };
+    }
+
+    addNumbers(args) {
+      return Number(args.NUM1) + Number(args.NUM2); // Make sure to return the result
+    }
   }
 
-  addNumbers(args) {
-    return Number(args.NUM1) + Number(args.NUM2);
-  }
-}
-
-Scratch.extensions.register(new MyExtension());
+  // Register the extension properly
+  Scratch.extensions.register(new MyExtension());
