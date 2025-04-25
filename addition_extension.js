c88f875abad86d041a91bb25cb91688df1202660
salmamifdal <salmamifdal72@gmail.com>
2025-04-25 13:08:04 -0400

    Add files via upload

--- addition_extension.js
+++ addition_extension.js
@@ -1,23 +1,35 @@
-class AdditionExtension {
-    getInfo() {
-        return {
-            id: 'additionExtension',
-            name: 'Addition',
-            blocks: [
-                {
-                    opcode: 'addOnePlusOne',
-                    blockType: Scratch.BlockType.REPORTER,
-                    text: '1 + 1',
-                    func: 'addOnePlusOne'
-                }
-            ]
-        };
+(function(Scratch) {
+    'use strict';
+
+    class AdditionExtension {
+        getInfo() {
+            return {
+                id: 'additionExtension', // unique ID
+                name: 'Addition Tools', // extension name in Scratch
+                blocks: [
+                    {
+                        opcode: 'addTwoNumbers',
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: 'add [NUM1] and [NUM2]',
+                        arguments: {
+                            NUM1: {
+                                type: Scratch.ArgumentType.NUMBER,
+                                defaultValue: 1
+                            },
+                            NUM2: {
+                                type: Scratch.ArgumentType.NUMBER,
+                                defaultValue: 1
+                            }
+                        }
+                    }
+                ]
+            };
+        }
+
+        addTwoNumbers(args) {
+            return Number(args.NUM1) + Number(args.NUM2);
+        }
     }
 
-    addOnePlusOne() {
-        return 2;
-    }
-}
-
-// This line registers the extension with Scratch
-Scratch.extensions.register(new AdditionExtension());
+    Scratch.extensions.register(new AdditionExtension());
+})(Scratch);
