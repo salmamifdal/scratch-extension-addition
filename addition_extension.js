24e29a7b9c3126b1befdc522d04e1bbc92d1a407
salmamifdal <salmamifdal72@gmail.com>
2025-04-25 13:30:11 -0400

    Add files via upload

--- addition_extension.js
+++ addition_extension.js
@@ -1,40 +1,31 @@
-(function(Scratch) {
-    'use strict';
-
-    if (!Scratch.extensions.unsandboxed) {
-        throw new Error('This extension requires unsandboxed mode.');
-    }
-
-    class AdditionExtension {
-        getInfo() {
-            return {
-                id: 'additionExtension',
-                name: 'Addition Tools',
-                blocks: [
-                    {
-                        opcode: 'addTwoNumbers',
-                        blockType: Scratch.BlockType.REPORTER,
-                        text: 'add [NUM1] and [NUM2]',
-                        arguments: {
-                            NUM1: {
-                                type: Scratch.ArgumentType.NUMBER,
-                                defaultValue: 1
-                            },
-                            NUM2: {
-                                type: Scratch.ArgumentType.NUMBER,
-                                defaultValue: 1
-                            }
-                        },
-                        func: 'addTwoNumbers'
-                    }
-                ]
-            };
+class MyExtension {
+  getInfo() {
+    return {
+      id: 'addition',
+      name: 'Addition',
+      blocks: [
+        {
+          opcode: 'addNumbers',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'add [NUM1] and [NUM2]',
+          arguments: {
+            NUM1: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 1
+            },
+            NUM2: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 2
+            }
+          }
         }
+      ]
+    };
+  }
 
-        addTwoNumbers(args) {
-            return Number(args.NUM1) + Number(args.NUM2);
-        }
-    }
+  addNumbers(args) {
+    return Number(args.NUM1) + Number(args.NUM2);
+  }
+}
 
-    Scratch.extensions.register(new AdditionExtension());
-})(Scratch);
+Scratch.extensions.register(new MyExtension());
