1e4e3d88574b3c784effa492607f991f193c189c
salmamifdal <salmamifdal72@gmail.com>
2025-04-25 14:55:01 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -18,13 +18,57 @@ class MyExtension {
                             defaultValue: 1
                         }
                     }
+                },
+                {
+                    opcode: 'addWithEmoji',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'add [A] and [B] with emoji',
+                    arguments: {
+                        A: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 2
+                        },
+                        B: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 3
+                        }
+                    }
+                },
+                {
+                    opcode: 'randomAddition',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'random add between [MIN] and [MAX]',
+                    arguments: {
+                        MIN: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 1
+                        },
+                        MAX: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 10
+                        }
+                    }
                 }
             ]
         };
     }
 
     addNumbers(args) {
-        return args.NUM1 + args.NUM2;
+        return Number(args.NUM1) + Number(args.NUM2);
+    }
+
+    addWithEmoji(args) {
+        const sum = Number(args.A) + Number(args.B);
+        return `${args.A} + ${args.B} = ${sum} 🎉`;
+    }
+
+    randomAddition(args) {
+        const min = Number(args.MIN);
+        const max = Number(args.MAX);
+        const num1 = Math.floor(Math.random() * (max - min + 1)) + min;
+        const num2 = Math.floor(Math.random() * (max - min + 1)) + min;
+        const sum = num1 + num2;
+        return `${num1} + ${num2} = ${sum}`;
     }
 }
 
